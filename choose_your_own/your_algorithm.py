e2b586bddfa3650c0cefc7bd9bc84d6982e29689
Vinicius Isola <viniciusisola@gmail.com>
2016-06-29 21:36:45 -0400

    Finished choose your own algorithm

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -17,25 +17,39 @@ bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 
 
 #### initial visualization
-plt.xlim(0.0, 1.0)
-plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
-plt.legend()
-plt.xlabel("bumpiness")
-plt.ylabel("grade")
-plt.show()
+# plt.xlim(0.0, 1.0)
+# plt.ylim(0.0, 1.0)
+# plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+# plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+# plt.legend()
+# plt.xlabel("bumpiness")
+# plt.ylabel("grade")
+# plt.show()
 ################################################################################
 
 
-### your code here!  name your classifier object clf if you want the 
+### your code here!  name your classifier object clf if you want the
 ### visualization code (prettyPicture) to show you the decision boundary
 
+from time import time
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
+clf = KNeighborsClassifier(n_neighbors = 18)
 
+print "Number of features:", len(features_train[0])
 
+print "Training..."
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+print "Testing..."
+t1 = time()
+predict = clf.predict(features_test)
+print "testing time:", round(time()-t0, 3), "s"
 
+print "accuracy:", accuracy_score(labels_test, predict)
 
 
 try:
